42ed3a0bc72e8811a86f97026754dff86f587835
Abdullah Al Kawser <abdullahalakwser1@gmail.com>
2024-04-21 13:16:12 -0700

    addd

---
+++ src/script.ts
@@ -0,0 +1,147 @@
+// const prices = 20090
+
+// console.log('heloww how are yougffghghhhhhhhh', prices)
+
+
+// const hanle = ()=>{
+//     console.log('arooo function')
+// }
+// hanle()
+
+
+
+
+//type of typscrift 
+
+
+// let plyer = 'masrafiii';
+// plyer = 980;  Type 'number' is not assignable to type 'string'
+// console.log(plyer)
+
+
+// let plyer = 'masrafiii';
+// let age= 90;
+
+// age = '980'; //Type 'string' is not assignable to type 'number'.
+// console.log(plyer)
+
+
+// let plyers; // declaretion lkore
+// plyers = 'kawser'; // assent koraa
+// plyers = 39;
+
+// console.log(plyers)
+
+
+// function 
+
+//a: number,b: number
+
+// function multipy(a: number,b: number) {
+//     return a*b
+    
+// }
+
+// console.log(multipy(3,4))
+
+
+/// Array 
+
+// let arryssss = ['dhaka','apple']
+// arryssss.push(23) //Argument of type 'number' is not assignable to parameter of 
+
+
+// let arryssss = []
+// arryssss.push(23) 
+// arryssss.push('abvdhggc') 
+
+// console.log(arryssss)
+
+//object 
+
+// let person = {
+//     name: 'abdullah',
+//     age: 120,
+//     country: 'bangladesh'
+// }
+// person.isloading = 'ture'// Property 'isloading' does not exist on type '{ name: string; age: number; country: string; }'.ts(2339)
+
+// Implicit Type
+// Implicit - TypeScript will "guess" the type, based on the assigned value:
+
+// let firstName = "Dylan";
+
+
+//Explicit Type
+
+
+// let firstName: string = "Dylan"; // type string
+
+// console.log(typeof firstName);
+
+
+
+//Array
+
+// const names: string[] = [];
+// names.push("Dylan"); 
+
+// const age: number[] = [];
+// age.push("Dylan"); //Argument of type 'string' is not assignable to parameter of type 'number'.ts(2345)
+
+
+// const a:( string | number)[] = [];
+// a.push("Dylan",78); 
+
+//object
+// let c : {
+//     name : string,
+//     age : number
+// }
+
+// let c : object;
+
+// c = [2,3,4,454,] // array is objcet
+
+
+// c= {
+//     name: 'abdullah',
+//     age:23
+
+// }
+
+
+
+// dynanic type  any
+
+// let a: any; //anny
+
+// a = "string";
+// a = 4;
+
+//array
+
+// let a: any [] =[];
+
+// a.push('bangladesh')
+// a.push(90)
+
+//object
+
+// let a: {
+//     name:any;
+//     age:any
+// };
+
+// a ={
+//     name: 90,
+//     age: 'banhjdg'
+
+// }
+
+
+
+
+
+
+
